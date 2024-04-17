f55fd01d868d7d634a612bacf8907d378e183d06
j-halladay5 <156850724+j-halladay5@users.noreply.github.com>
2024-04-16 20:02:10 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,39 @@
-function hasCycle(graph) {
+function detectCycles(graph)
+{
+    for(var key in graph)
+    {
+        var path = [];
+        
+        if(smallerCycles(graph, key, path))
+        {
+            return true;
+        }
+    }
     return false;
 }
+
+function smallerCycles(graph, key, path=[])
+{
+    path.push(key);
+    for(var pathNode in graph[key])
+    {
+        if(!path.includes(pathNode))
+        {
+            var smaller = smallerCycles(graph, pathNode, path);
+                
+            if(!smaller)
+            {
+                path = [];
+                path.push(key);
+                
+            }else{
+                return true;
+            }
+        }else{
+            return true;
+        }
+        
+    }
+    
+    return false;
+}   
