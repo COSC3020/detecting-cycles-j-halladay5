680433406ed119f534bd8b17eba46ea9a084f920
j-halladay5 <156850724+j-halladay5@users.noreply.github.com>
2024-04-16 20:42:43 -0600

    Update code.test.js add )

--- code.test.js
+++ code.test.js
@@ -8,7 +8,7 @@ var test1 = {"W" : {"X" : 1},
              "X" : {"Y" : 1},
              "Y" : {"Z" :1 },
              "Z" :{}};
-assert(JSON.stringify(detectCycles(test1) == JSON.stringify(false));
+assert(JSON.stringify(detectCycles(test1) == JSON.stringify(false)));
 
 
 //random with cycles & disconnected node
@@ -18,7 +18,7 @@ var test2 = { "Disconnected" :{},
              "C" : {},
              "D" : {"B":1, "E":2},
              "E" : {"C":5}};
-assert(JSON.stringify(detectCycles(test2) == JSON.stringify(true));
+assert(JSON.stringify(detectCycles(test2) == JSON.stringify(true)));
 
 
 //random no cycles & disconnected node
@@ -28,7 +28,7 @@ var test3 = {"Disconnected" :{},
              "C" : {},
              "D" : {"B":1, "E":2},
              "E" : {"C":5}};
-assert(JSON.stringify(detectCycles(test3) == JSON.stringify(false));
+assert(JSON.stringify(detectCycles(test3) == JSON.stringify(false)));
 
 
 //complete graph with cycles
@@ -36,16 +36,16 @@ var test4 = {"A" : {"B" : 2, "C" : 3, "D" : 1},
              "B" : {"A" : 2, "C" : 3, "D" : 1},
              "C" : {"A" : 2, "B" : 3, "D" : 1},
              "D" : {"A" : 2, "B" : 3, "C" : 1}};
-assert(JSON.stringify(detectCycles(test4) == JSON.stringify(true));
+assert(JSON.stringify(detectCycles(test4) == JSON.stringify(true)));
 
 
 //single node with cycle
 var test5 = {'A': {}};
-assert(JSON.stringify(detectCycles(test5) == JSON.stringify(true));
+assert(JSON.stringify(detectCycles(test5) == JSON.stringify(true)));
 
 
 //circle with cycles
 var test6 = {'B' : {'C' :2},
              'C' : {'D' :2},
              'D' : {'B': 2}};
-assert(JSON.stringify(detectCycles(test6) == JSON.stringify(true));
+assert(JSON.stringify(detectCycles(test6) == JSON.stringify(true)));
